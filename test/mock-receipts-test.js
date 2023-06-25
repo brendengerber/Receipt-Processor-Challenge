e5f71b0834a39e1f516b89fd9c6071f3726ffd5a
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-25 16:41:35 -0500

    Add arrays to mock-receipts-test.js to fill with mock data

---
+++ test/mock-receipts-test.js
@@ -0,0 +1,10 @@
+//Preformatted mock data to use for testing
+let validMockReceipts = [];
+let invalidMockReceipts = [];
+let emptyMocReceipts = [];
+
+module.exports = {
+    validMockReceipts,
+    invalidMockReceipts,
+    emptyMocReceipts
+};
