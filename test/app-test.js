404e72c7e8bd141492e26805df45270b06036aa1
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 18:29:15 -0500

    Add more content to testing suite

--- test/app-test.js
+++ test/app-test.js
@@ -1,10 +1,37 @@
 //Imports necessary modules
 const request = require('supertest');
+const {assert} = require('chai');
+const expect = require('chai').expect;
+const app = require('../app.js');
 
-describe('/receipts GET request', () => {
-    it('', async () => {
-      
+//Testing suite
+describe('app', () => {
+    describe('/receipts', () => {
+        describe('/process POST request', () => {
+            it('it should generate a v4 UUID for the new receipt and return it as a JSON object', async () => {
+                const response = await request(app).post('/receipts/process');
+                
 
+
+
+                
+            });
+            it('it should send an error message if the receipt object is not properly formatted', async () => {
+
+            });
+        });
     });
-})
+});
 
+
+//post
+// should not POST the receipt if it is formatted incorrectly
+// should POST a correctly formatted receipt and send the newly posted receipt
+
+
+//test the test receipts to make sure they come to the correct points
+
+//for get id
+//it('it should send the proper ERROR message if the resource does not exist', async () => {});
+
+//responses are type json
