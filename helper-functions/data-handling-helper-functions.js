1aeb86ed437ab0832eaa61e95208dab7ca89b5e3
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 13:52:34 -0500

    Add assigneEntryId function to data-handling-helper-functions.js

--- helper-functions/data-handling-helper-functions.js
+++ helper-functions/data-handling-helper-functions.js
@@ -1,12 +1,33 @@
-//Helper functions for reuse in middleware functions to access and manipulate the data
-//These functions return false if the resource does not exist which allows middleware to check existance using an if statement and send a 404 if it does not exist
-//These functions are generic and take a set of data as an argument so they can be used for receipts or any other data sets that may be added in the future
-//These functions can be refactored if the location of the data is changed, allowing the middleware and routers to continue functioning normally (assuming the new functions here return the same results as the old)
+//Imports necessary modules
+const crypto = require('crypto')
 
-//Returns data or returns false if no data exists
-const getData = function(data){
-    if(data.length === 0){
-        return false
+const assignEntryId = function(data){
+    let id;
+    let unique = false;
+    //Continues generating UUIDv4s until a unique one is generated
+    while(!unique){
+        id = crypto.randomUUID();
+        unique = !data.hasOwnProperty(id);
     }
-    return data
-}
+    //Reserves the UUIDv4 so it cannot be taken by another request coming in before processing has been completed
+    data.id = undefined;
+    //Returns the UUIDv4 for use in consequent functions and middleware
+    return id;
+};
+
+module.exports = {
+    assignEntryId
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
