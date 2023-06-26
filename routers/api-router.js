b9e91f73873addd4275797948d4bc1aad1af67df
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 17:15:24 -0500

    Move error handling to api-router

--- routers/api-router.js
+++ routers/api-router.js
@@ -8,5 +8,13 @@ const apiRouter = express.Router();
 const receiptsRouter = require('./routers/receipts-router.js');
 apiRouter.use('/receipts', receiptsRouter)
 
+//Handles errors
+apiRouter.use((err, req, res, next) => {
+    if(!err.status){
+      err.status = 500;
+    }
+    res.status(err.status).send(err.message);
+  })
+
 //Exports the router
 module.exports = apiRouter;
