976bb6a22c46048433e25e57dca228b1109e6312
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 18:28:08 -0500

    Update comments in data-handling-helper-functions.js

--- helper-functions/data-handling-helper-functions.js
+++ helper-functions/data-handling-helper-functions.js
@@ -1,30 +1,23 @@
-//Helper functions for reuse in middleware functions to access and manipulate data objects
-//These functions return false if the resource does not exist which allows middleware to check existance using an if statement and send a 404 if it does not exist
-//These functions are generic and take a set of data as an argument so they can be used for receipts or any other data sets that may be added in the future
-//These functions can be refactored if the location of the data is changed, allowing the middleware and routers to continue functioning normally (assuming the new functions here return the same results as the old)
-
-//The properties of a data object should be UUIDv4s that coorispond to a specific entry (such as a receipt)
-
 //Imports necessary modules
 const crypto = require('crypto')
 
-//Assigns a UUIDv4 to an entry for any data object
+//Assigns a v4 UUID to an entry for any data object
 const assignEntryId = function(data){
     let id;
     let unique = false;
-    //Continues generating UUIDv4s until a unique one is generated
+    //Continues generating v4 UUIDs until a unique one is generated
     while(!unique){
         id = crypto.randomUUID();
         unique = !data.hasOwnProperty(id);
     }
-    //Reserves the UUIDv4 so it cannot be taken by another request coming in before processing has been completed
+    //Reserves the v4 UUID so it cannot be taken by another request coming in before processing has been completed
     data.id = undefined;
-    //Returns the UUIDv4 for use in consequent functions and middleware
+    //Returns the v4 UUID for use in consequent functions and middleware
     return id;
 };
 
-//Assigns a UUIDv4 to a new entry, adds it to the data object, and returns the newly added entry
-const postEntry = function(data, entry){
+//Assigns a v4 UUID to a new entry, adds it to the data object, and returns the newly added entry
+const addEntry = function(data, entry){
     const id = assignEntryId(data);
     entry.id = id;
     data[id] = entry;
@@ -37,7 +30,7 @@ const calculatePoints = function(data){
 
 module.exports = {
     assignEntryId,
-    postEntry,
+    addEntry,
     calculatePoints
 };
 
