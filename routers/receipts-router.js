ded4d0adeff2e89b5a0557cb69fbef6f9ea6f804
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 17:37:55 -0500

    Add /prossess POST route to receipt-router.js

--- routers/receipts-router.js
+++ routers/receipts-router.js
@@ -1,6 +1,18 @@
 //Imports necessary modules
 const express = require('express');
-const receipts = require('../data/receipts.js');
+const validateReceipt = require('../middleware/validation-middleware.js');
+const {assignReceiptId, addReceipt} = require('../middleware/database-middleware.js');
+
+
+//Creates the router
+const receiptsRouter = express.Router();
+
+//Assigns the new receipt a v4 UUID and POSTs it
+receiptsRouter.post('/process', validateReceipt, assignReceiptId, addReceipt, (req, res, next) => {
+    res.status(201).JSON({"id": req.receipt.id});
+});
 
 
 
+//Exports the router
+module.exports = receiptsRouter;
