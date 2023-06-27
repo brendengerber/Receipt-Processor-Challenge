0a9ed6873f59f21c2c594e36d653e50f8a55c4ab
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 20:06:47 -0500

    Add calculatePoints function to data-handling-functions.js

--- helper-functions/data-handling-helper-functions.js
+++ helper-functions/data-handling-helper-functions.js
@@ -33,13 +33,42 @@ const findEntry = function(data, id){
     }
 };
 
-const calculatePoints = function(data){
-
+const calculatePoints = function(receipt){
+    let points = 0;
+    //Adds 1 point for every alphanumeric character in the retailer name
+    points += receipt.retailer.replace(/[^0-9a-z]/gi, '').length;
+    //Adds 50 points if the total is a round dollar amount with no cents
+    if(Number.isInteger(Number(receipt.total))){
+        points += 50;
+    }
+    // Adds 25 points if the total is a multiple of .25
+    if(Number(receipt.total) % .25 === 0){
+        points += 25;
+    }
+    //Adds 5 points for every two items on the receipt
+    points += Math.floor((receipt.items.length)/2) * 5;
+    //Adds points equal to the price of the item multiplied by .2 and rounded up if the trimmed length of the description is a multiple of 3
+    for(let item of receipt.items){
+        if(item.shortDescription.trim().length % 3 === 0){
+            points += Math.ceil(Number(item.price) * .2);
+        }
+    }
+    //Adds 6 points if the day in the purchase date is odd
+    if(receipt.purchaseDate.slice(-2) % 2 !== 0){
+        points += 6;
+    }
+    //Adds 10 points if the time of purchase is after 2:00pm and before 4:00pm
+    const hourOfPurchase = receipt.purchaseTime.slice(0,2);
+    if(hourOfPurchase >= 14 && hourOfPurchase <= 15){
+        points += 10;
+    }
+    return points;
 };
 
 module.exports = {
     assignEntryId,
     addEntry,
     calculatePoints,
-    findEntry
+    findEntry,
+    calculatePoints
 };
