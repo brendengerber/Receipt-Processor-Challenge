e0b9ccc40c72bf1b49dd2cc5a34a0a0d06de3ea4
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-26 19:07:27 -0500

    Add status code to validateIdParam in validation-middleware

--- middleware/validation-middleware.js
+++ middleware/validation-middleware.js
@@ -39,18 +39,35 @@ const validateReceipt = (req, res, next) => {
         if(validationErrors.length === 0){
             req.receipt = req.body;
             next();
-        //Creates an error describing the invalidities and passes it to the error handling middleware
+        //Creates an error describing the invalid formatting and passes it to the error handling middleware
         }else{
-            const err = new Error(`Invalid receipt format.\n ${validationErrors.join("\n")}`);
+            const err = new Error(`The receipt format is invalid.\n ${validationErrors.join("\n")}`);
             err.status = 403;
             next(err);
         }
     //Catches any other unforseen server errors and passes them to the error handling middleware
-    } catch(err){
-       next(err)
+    }catch(err){
+       next(err);
+    }
+};
+
+const validateIdParam = (req, res, next) => {
+    try{
+        //Uses a regular expression to test that the Id of the requested receipt is a v4 UUID
+        if(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(req.params.id)){
+            req.id = req.params.id;
+            rext();
+        }else{
+            const err = new Error('Request parameter is not a v4 UUID.');
+            err.status = 400;
+            next(err);
+        }
+    }catch(err){
+        next(err);
     }
 };
 
 module.exports = {
-    validateReceipt
+    validateReceipt,
+    validateIdParam
 };
