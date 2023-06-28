93f76b86648cf2ca6e4def08f90e35f1f0327bb4
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-28 13:45:38 -0500

    Refactor middleware and helper functions to catch more errors

--- helper-functions/data-handling-helper-functions.js
+++ helper-functions/data-handling-helper-functions.js
@@ -5,27 +5,34 @@ const {data} = require('../data/data.js');
 
 //Creats a v4 UUID to an entry for any data object, dataSet is a string to identify which dataSet to access
 const createEntryId = function(dataSet){
-    let newId;
-    let unique = false;
-    //Continues generating v4 UUIDs until a unique one is generated
-    while(!unique){
-        newId = crypto.randomUUID();
-        unique = !data["get"+dataSet]().hasOwnProperty(newId);
+    try{
+        let newId;
+        let unique = false;
+        //Continues generating v4 UUIDs until a unique one is generated
+        while(!unique){
+            newId = crypto.randomUUID();
+            unique = !data["get"+dataSet]().hasOwnProperty(newId);
+        }
+        //Reserves the v4 UUID so it cannot be taken by another request coming in before processing has been completed
+        data["save"+dataSet.slice(0, -1)]({id: newId});
+        //Returns the v4 UUID for use in consequent functions and middleware
+        return newId;
+    }catch(err){
+        throw err;
     }
-    //Reserves the v4 UUID so it cannot be taken by another request coming in before processing has been completed
-    data["save"+dataSet.slice(0, -1)]({id: newId});
-    //Returns the v4 UUID for use in consequent functions and middleware
-    return newId;
 };
 
 //Adds an entry to the data object, dataSet is a string to identify which dataSet to access
 const addEntry = function(dataSet, entry){
-    data["save"+dataSet.slice(0, -1)](entry);
-    return true;
+    try{
+        data["save"+dataSet.slice(0, -1)](entry);
+    }catch(err){
+        throw err;
+    }
 };
 
 const findEntry = function(dataSet, id){
-    let entry = data["get"+dataSet]()[id]
+    let entry = data["get"+dataSet]()[id];
     if(entry){
         return entry;
     }else{
@@ -34,35 +41,40 @@ const findEntry = function(dataSet, id){
 };
 
 const calculateReceiptPoints = function(receipt){
-    let points = 0;
-    //Adds 1 point for every alphanumeric character in the retailer name
-    points += receipt.retailer.replace(/[^0-9a-z]/gi, '').length;
-    //Adds 50 points if the total is a round dollar amount with no cents
-    if(Number.isInteger(Number(receipt.total))){
-        points += 50;
-    }
-    // Adds 25 points if the total is a multiple of .25
-    if(Number(receipt.total) % .25 === 0){
-        points += 25;
-    }
-    //Adds 5 points for every two items on the receipt
-    points += Math.floor((receipt.items.length)/2) * 5;
-    //Adds points equal to the price of the item multiplied by .2 and rounded up if the trimmed length of the description is a multiple of 3
-    for(let item of receipt.items){
-        if(item.shortDescription.trim().length % 3 === 0){
-            points += Math.ceil(Number(item.price) * .2);
+    try{
+        let points = 0;
+        //Adds 1 point for every alphanumeric character in the retailer name
+        points += receipt.retailer.replace(/[^0-9a-z]/gi, '').length;
+        //Adds 50 points if the total is a round dollar amount with no cents
+        if(Number.isInteger(Number(receipt.total))){
+            points += 50;
         }
+        // Adds 25 points if the total is a multiple of .25
+        if(Number(receipt.total) % .25 === 0){
+            points += 25;
+        }
+        //Adds 5 points for every two items on the receipt
+        points += Math.floor((receipt.items.length)/2) * 5;
+        //Adds points equal to the price of the item multiplied by .2 and rounded up if the trimmed length of the description is a multiple of 3
+        for(let item of receipt.items){
+            if(item.shortDescription.trim().length % 3 === 0){
+                points += Math.ceil(Number(item.price) * .2);
+            }
+        }
+        //Adds 6 points if the day in the purchase date is odd
+        if(receipt.purchaseDate.slice(-2) % 2 !== 0){
+            points += 6;
+        }
+        //Adds 10 points if the time of purchase is after 2:00pm and before 4:00pm
+        const hourOfPurchase = receipt.purchaseTime.slice(0,2);
+        if(hourOfPurchase >= 14 && hourOfPurchase <= 15){
+            points += 10;
+        }
+        return points;
+    }catch(err){
+        throw err;
     }
-    //Adds 6 points if the day in the purchase date is odd
-    if(receipt.purchaseDate.slice(-2) % 2 !== 0){
-        points += 6;
-    }
-    //Adds 10 points if the time of purchase is after 2:00pm and before 4:00pm
-    const hourOfPurchase = receipt.purchaseTime.slice(0,2);
-    if(hourOfPurchase >= 14 && hourOfPurchase <= 15){
-        points += 10;
-    }
-    return points;
+
 };
 
 module.exports = {
--- helper-functions/validation-helper-functions.js
+++ helper-functions/validation-helper-functions.js
@@ -2,41 +2,57 @@
 const validator = require('validator');
 
 const validateDate = function(dateString){
-    if(typeof dateString !== 'string'){
-        return undefined;
+    try{
+        if(typeof dateString !== 'string'){
+            return undefined;
+        }
+        if(!validator.isDate(dateString, {format: 'YYYY-MM-DD', delimiters: ['-']})){
+            return false;
+        }
+        return true;
+    }catch(err){
+        throw err;
     }
-    if(!validator.isDate(dateString, {format: 'YYYY-MM-DD', delimiters: ['-']})){
-        return false;
-    }
-    return true;
 };
 
 const validateTime = function(timeString){
-    if(typeof timeString !== 'string'){
-        return undefined;
+    try{
+        if(typeof timeString !== 'string'){
+            return undefined;
+        }
+        if(!validator.isTime(timeString, {hourFromat: 'hour24'})){
+            return false;
+        }
+        return true;
+    }catch(err){
+        throw err;
     }
-    if(!validator.isTime(timeString, {hourFromat: 'hour24'})){
-        return false;
-    }
-    return true;
 };
 
 const validatePrice = function(priceString){
-    if(typeof priceString !== 'string'){
-        return undefined;
-    };
-    if(!validator.isCurrency(priceString, {thousands_separator: '', require_decimal: true, digits_after_decimal: [2]}) || isNaN(priceString)){
-        return false;
-    };
-    return true;
+    try{
+        if(typeof priceString !== 'string'){
+            return undefined;
+        };
+        if(!validator.isCurrency(priceString, {thousands_separator: '', require_decimal: true, digits_after_decimal: [2]}) || isNaN(priceString)){
+            return false;
+        };
+        return true;
+    }catch(err){
+        throw err;
+    }
 };
 
 const validateId = function(id){
-    if(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(id)){
-        return true
+    try{
+        if(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(id)){
+            return true;
+        }
+        return false;
+    }catch(err){
+        throw err;
     }
-    return false
-}
+};
 
 module.exports = {
     validateDate,
