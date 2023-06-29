1aed386f06bc072a49b3d4403050cc26ad0e0b03
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-06-29 11:24:52 -0500

    Add param method to receiptsRouter

--- routers/receipts-router.js
+++ routers/receipts-router.js
@@ -6,15 +6,20 @@ const {assignReceiptId, assignReceiptPoints, addReceipt, attatchReceiptById} = r
 //Creates the router
 const receiptsRouter = express.Router();
 
+//Validates the Id of all routes with an Id parameter
+receiptsRouter.param('id', validateIdParam);
+
 //Assigns the new receipt a v4 UUID, assigns its point value, saves it, and sends a response with the newly assigned Id
+//For a successful POST, the body must be a properly formatted receipt in JSON
 receiptsRouter.post('/process', validateReceipt, assignReceiptId, assignReceiptPoints, addReceipt, (req, res, next) => {
     res.status(201).send({"id": req.receipt.id});
 });
 
 //Looks up the receipt of the given Id parameter and sends a response with the calculated points value
-receiptsRouter.get('/:id/points', validateIdParam, attatchReceiptById, (req, res, next) => {
+//For a successful GET, the Id parameter must be a valid v4 UUID that exists in the dataset
+receiptsRouter.get('/:id/points', attatchReceiptById, (req, res, next) => {
     res.status(200).send({"points": req.receipt.points});
-})
+});
 
 //Exports the router
 module.exports = receiptsRouter;
